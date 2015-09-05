bc0f3712b27f01c6718f86aab20295c685c2bae1
shigarus <shigarus@rambler.ru>
2015-09-05 16:03:00 +0300

    Added skeleton for cmphtml module

---
+++ src/cmphtml.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+def return_diff(tree1, tree2):
+    """
+    :param tree1: html tree
+    :param tree2: html tree
+    :return: html tree
+    """
+    pass
+
+
+def return_equivalent(tree1, tree2):
+    """
+    :param tree1: html tree
+    :param tree2: html tree
+    :return: html tree
+    """
+    pass
