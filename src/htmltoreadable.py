11ba43dde010e95ee14728c509ee7b5de247a903
shigarus <shigarus@rambler.ru>
2015-09-07 01:59:35 +0300

    Added comments

--- src/htmltoreadable.py
+++ src/htmltoreadable.py
@@ -32,6 +32,9 @@ def html_to_readable(element):
     """
     if not isinstance(element, lxml.html.HtmlElement):
         raise TypeError('element has to be lxml.html.HtmlElement instance')
+
+    # need to process inner <a> tags
+    # to avoid collisions with adding url after <a> tag
     for node in element.cssselect('a'):
         href = node.get('href')
         if not href:
@@ -43,6 +46,7 @@ def html_to_readable(element):
             href,
             tail
         ))
+        
     inner_tags_to_text(element)
     return element.text
 
@@ -57,10 +61,13 @@ def inner_tags_to_text(element):
     paragraphs = []
     cur_str = u''
     for node in element.iter():
+        # need tail only for inner tag
         if node is element:
             tail = u''
         else:
             tail = node.tail or u''
+
+        # check if tag closes current paragraph
         tag = node.tag
         if tag == 'br':
             paragraphs.append(cur_str)
@@ -70,20 +77,25 @@ def inner_tags_to_text(element):
             if tag != 'li':
                 paragraphs.append(u'')
             cur_str = u''
+
         cur_str = ''.join([
             cur_str,
             node.text or u'',
             tail
         ])
     paragraphs.append(cur_str)
+
+    # need to delete inner tags to not include their text again
+    # if we will use this fnc on parent of the current tag
+    for node in element.iter():
+        if node is not element:
+            node.getparent().remove(node)
+
     res = u'\r\n'.join([
         word_wrap(text)
         for text in paragraphs
     ])
     res = re.sub(MANY_LINE_ENDINGS, '\r\n\r\n', res)
-    for node in element.iter():
-        if node is not element:
-            node.getparent().remove(node)
     element.text = res
 
 
@@ -101,12 +113,16 @@ def word_wrap(text):
     lines = []
     while len(text) > 80:
         last_space = text[:80].rfind(' ')
+
+        # if word large then 80 symbols
         if last_space == -1:
             last_space = text.find(' ')
         if last_space == -1:
             break
+
         line, text = text[:last_space], text[last_space+1:]
         lines.append(line)
+
     lines.append(text)
     lines.append('\r\n')
     return u'\r\n'.join(lines)
